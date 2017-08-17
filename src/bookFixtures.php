01256895a4538dc9393864562d1c5bfaf2e6368c
gael <iceberg007@live.fr>
2017-08-17 16:51:09 +0200

    creation et  beer et knd  et une table fk: kindsbeers

--- src/bookFixtures.php
+++ src/bookFixtures.php
@@ -2,15 +2,52 @@
 require "redbean-boostrap.php";
 use RedBeanPHP\R;
 
-$book=R::dispense("book");
-$book->tilte="les chants de Maldoror";
-$book->author="Lautréamont";
-
-R::store($book);
+R::wipe("book");
+R::trashAll(R::findAll("beer"));
+R::trashAll(R::findAll("beerkind"));
 
 $book=R::dispense("book");
-S
 $book->tilte="les Refrets";
 $book->author="Joaquim Du Bellay";
 $book->genre="Poésie";
 R::store($book);
+
+$kinds= R::dispense("beerkind",3);
+$kinds[0]->kind="blonde";
+$kinds[1]->kind="brune";
+$kinds[2]->kind="noire";
+
+$beers= R::dispense("beer",5);
+$beers[0]->name="Leffe";
+$beers[0]->kind=$kinds[0];
+$beers[0]->strength=6.5;
+$kinds[0]->ownBeer[]=$beers[0];
+
+$beers[1]->name="Guiness";
+$beers[1]->kind=$kinds[1];
+$beers[1]->strength=7;
+$kinds[2]->ownBeer[]=$beers[1];
+
+$beers[2]->name="Primus";
+$beers[2]->kind=$kinds[0];
+$beers[2]->strength=5.7;
+$kinds[0]->ownBeer[]=$beers[3];
+
+$beers[3]->name="Heineken";
+$beers[3]->kind=$kinds[0];
+$beers[3]->strength=5;
+$kinds[0]->ownBeer[]=$beers[3];
+
+$beers[4]->name="chimay Bleue";
+$beers[4]->kind=$kinds[1];
+$beers[4]->strength=5;
+$kinds[2]->ownBeeer[]=$beers[4];
+
+R::storeAll($beers);
+R::storeAll($kinds);
+
+
+
+echo "il y a ".count($kinds[0]->ownBeer).'blonde \n';
+$keys=array_keys($kinds[0]->with("ORDER BY name DESC")->ownBeer);
+echo "la premiere est ".$kinds[0]->ownBeer[$keys[0]]->name."\n";
